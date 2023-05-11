5240cef176ec5d12211432bcb82553ff991e218e
Kajaia <lashakajaia31@gmail.com>
2023-05-11 23:23:50 +0300

    Added sentry for error reporting

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://5d6600021f4843f594cf0284a71915e0@o4505167174696960.ingest.sentry.io/4505167175811072",
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
